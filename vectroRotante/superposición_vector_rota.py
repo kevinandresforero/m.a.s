d822db6313ffa65c76db285dff973b666818a70c
hkevin <kevinandresforero@gmail.com>
2024-04-04 06:27:56 -0500

    Ejercicio parcial

--- vectroRotante/superposición_vector_rota.py
+++ vectroRotante/superposición_vector_rota.py
@@ -3,11 +3,11 @@ import matplotlib.pyplot as plt
 
 # Parámetros de los péndulos
 A1 = 2  # Amplitud del péndulo 1
-A2 = 3  # Amplitud del péndulo 2
-w1 = 3  # Frecuencia angular del péndulo 1
-w2 = 4  # Frecuencia angular del péndulo 2
-phi1 = 0  # Fase del péndulo 1
-phi2 = np.pi/2  # Fase del péndulo 2
+A2 = 2*A1  # Amplitud del péndulo 2
+w1 = 2  # Frecuencia angular del péndulo 1
+w2 = 3  # Frecuencia angular del péndulo 2
+phi1 = np.pi/3  # Fase del péndulo 1
+phi2 = 0  # Fase del péndulo 2
 
 # Rango de tiempo
 t = np.linspace(0, 10*np.pi, 1000)
@@ -26,7 +26,7 @@ y_c1 = A1 * np.sin(theta_x)
 axs[1, 1].add_artist(plt.Circle((0, 0), A1, fill=False))
 axs[1, 1].plot(x_c1, y_c1, 'o-', color='red')
 axs[1, 1].set_xlabel('t')
-axs[1, 1].set_ylabel('x = A1 * sen(wt)')
+axs[1, 1].set_ylabel('x = A1 * sen(w1t + phi1)')
 
 # Subplot 2: Figura de Lissajous
 axs[0, 1].plot(x, y)
@@ -38,7 +38,7 @@ axs[0, 0].add_artist(plt.Circle((0, 0), A2, fill=False))
 x_c2 = A2 * np.cos(w2 * t + phi2)
 y_c2 = A2 * np.sin(w2 * t + phi2)
 axs[0, 0].plot(x_c2, y_c2, 'o-', color='blue')
-axs[0, 0].set_ylabel('y = A2 * sen(wt + π/2)')
+axs[0, 0].set_ylabel('y = A2 * sen(w2t + phi2)')
 
 # Ajustar espacio entre subplots
 plt.subplots_adjust(hspace=0.5, wspace=0.5)
